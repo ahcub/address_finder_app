61bbaa2b62251f3115ff3531b619c3c4b60f430d
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2016-01-04 15:57:10 +0200

    !U improve patterns speed (remove \n)

--- regexps_examples.py
+++ regexps_examples.py
@@ -1,11 +1,10 @@
 import re
 
 regexps = [
-        r'(?P<house_number>\w+) (?P<street_direction>(N|NW|NE|S|SW|SE))? (?P<street_name>[\w ]+)\s+(?P<locality>\w+), (?P<province_abbreviation>\w+) (?P<postal_code>\w+)\s+(?P<country>(united states|USA))',
+        r'(?P<house_number>\w+) (?P<street_direction>(N|NW|NE|S|SW|SE))? (?P<street_name>[\w ]+)(\s+)?(?P<locality>\w+), (?P<province_abbreviation>\w+) (?P<postal_code>\w+)(\s+)?(?P<country>(united states|USA))',
         r'(?P<house_number>\w+) (?P<street_direction>(N|NW|NE|S|SW|SE))? (?P<street_name>[\w ]+), (?P<building>[\w\s]+)\s+(?P<locality>\w+), (?P<province_abbreviation>\w+) (?P<postal_code>\w+)\s+(?P<country>(united states|USA))',
-        r'(?P<street_name>[\w\. ]+) (?P<building>[\w –]+)?\s(?P<postal_code>\w+) (?P<city>[\w -]+)',
-        r'(?P<street_name>[\w\. ]+) (?P<building>[\w -]+)?\s(?P<postal_code>[\w-]+) (?P<city>[\w -]+),? ?(?P<country>\w+)?',
-        r'(?P<street_name>[\w\. ]+), NO. (?P<building>\w+), (?P<apartment>[\w ]+)\s(?P<district>[\w ]+)\s(?P<city>[\w -]+)\s(?P<country>\w+)',
+        r'(?P<street_name>[\w\. ]+) (?P<building>[\w –]+)?\s?(?P<postal_code>\d\d\d\d+) (?P<city>[\w -]+)\s?',
+        r'(?P<street_name>[\w\. ]+), NO\. (?P<building>\w+), (?P<apartment>[\w ]+)(\s+)?(?P<district>[\w ]+)(\s+)?(?P<city>[a-z -]+)\s(?P<country>[a-z]+(\s+)?)',
 ]
 
 regexps = [re.compile(re_str, re.I) for re_str in regexps]
